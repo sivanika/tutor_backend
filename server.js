765f960352072f6486ba47e577c17fb13ad0d92a
sivanika <sivanika2025@gmail.com>
2026-01-22 12:42:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const server = http.createServer(app)
 // ✅ THEN CREATE SOCKET.IO WITH SAME SERVER
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://tutor-frontend-j2r5ojmp9-sivanikas-projects.vercel.app/",
     methods: ["GET", "POST"],
   },
 })
