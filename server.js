1c2e1d6dc0ef9133c0fa3137d28e864f0b1a6fba
sivanika <sivanika2025@gmail.com>
2026-03-05 12:58:13 +0530

    api update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express()
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://tutor-hours-frontend.vercel.app",
+  "https://tutor-frontend-ten.vercel.app/",
   process.env.CLIENT_URL
 ].filter(Boolean);
 
