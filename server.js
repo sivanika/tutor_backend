c9f62979c155f6bdc66aab6f5f268dee0981fc08
sivanika <sivanika2025@gmail.com>
2026-03-05 14:12:07 +0530

    api update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express"
 import http from "http"
 import { Server } from "socket.io"
 import cors from "cors"
+import morgan from "morgan"
 import connectDB from "./config/db.js"
 import adminRoutes from "./routes/adminRoutes.js"
 // routes
@@ -21,6 +22,7 @@ connectDB()
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(morgan("dev"))
 
 // routes
 app.use("/api/auth", authRoutes)
